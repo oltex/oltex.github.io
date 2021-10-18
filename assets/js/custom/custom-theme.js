de8d8ba9a5700c748d7bd54c867aa4eef28c83ce
oltex <80347929+oltex@users.noreply.github.com>
2021-10-18 23:27:03 +0900

    Update custom-theme.js

--- assets/js/custom/custom-theme.js
+++ assets/js/custom/custom-theme.js
@@ -1,1 +1,35 @@
+var defaultTheme = [...document.styleSheets].find(style => /(main.css)$/.test(style.href))
+var darkTheme = [...document.styleSheets].find(style => /(custom-main.css)$/.test(style.href))
 
+if (darkTheme) {
+    const darkModeCookie = document.cookie
+        .split('; ')
+        .find(co => co.startsWith('MDARK='))
+    if (darkModeCookie !== undefined) {
+        const dmodeValue = darkModeCookie.split('=')[1]
+        darkTheme.disabled = dmodeValue !== 'Y'
+        defaultTheme.disabled = dmodeValue === 'Y'
+    } else {
+        if (matchMedia('(prefers-color-scheme: dark)').matches) {
+            darkTheme.disabled = false
+            defaultTheme.disabled = true
+        } else {
+            darkTheme.disabled = true
+            defaultTheme.disabled = false
+        }
+        document.cookie = `MDARK=${darkTheme.disabled ? 'N' : 'Y'}; path=/;`
+    }
+
+    let toggleThemeBtn = document.getElementById("custom_theme_toggle")
+    if (toggleThemeBtn) {
+        toggleThemeBtn.checked = defaultTheme.disabled
+    }
+
+    let changeTheme = () => {
+        darkTheme.disabled = !darkTheme.disabled
+        defaultTheme.disabled = !darkTheme.disabled
+        document.cookie = `MDARK=${darkTheme.disabled ? 'N' : 'Y'}; path=/;`
+    }
+
+    toggleThemeBtn.addEventListener('click', changeTheme)
+}
